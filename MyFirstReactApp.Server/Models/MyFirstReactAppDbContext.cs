df343e9fd662d430f3dd543f4a1449487ff9159e
Fagudelo <ferney.certifications@outlook.com>
2024-08-27 00:18:02 -0500

    Se mapea la tabla 'Contacto' en la carpeta 'Models'con Scaffold-DbContext

---
+++ MyFirstReactApp.Server/Models/MyFirstReactAppDbContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyFirstReactApp.Server.Models;
+
+public partial class MyFirstReactAppDbContext : DbContext
+{
+    public MyFirstReactAppDbContext()
+    {
+    }
+
+    public MyFirstReactAppDbContext(DbContextOptions<MyFirstReactAppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Contacto> Contactos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Contacto>(entity =>
+        {
+            entity.HasKey(e => e.IdContacto).HasName("PK__Contacto__A4D6BBFA434C3336");
+
+            entity.ToTable("Contacto");
+
+            entity.Property(e => e.Correo)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Telefono)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
